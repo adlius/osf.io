271e6014918f94f478768069282842c59d8999c2
Yuhuai Liu <yuhuailiu@Admins-MacBook-Pro-4.local>
2018-02-27 15:26:54 -0500

    some changes

--- api/subscriptions/views.py
+++ api/subscriptions/views.py
@@ -1,18 +1,27 @@
 from rest_framework import generics
+from rest_framework.exceptions import NotFound
 
 from api.base.views import JSONAPIBaseView
-from api.base.utils import get_object_or_error
-from osf.models import NotificationSubscription, PreprintProvider
+from api.subscriptions.serializers import UserProviderSubscriptionSerializer
+from osf.models import NotificationSubscription, PreprintProvider, OSFUser
 
 
-class UserProviderSubscriptionDetail(JSONAPIBaseView, generics.RetrieveAPIView):
+class UserProviderSubscriptionDetail(JSONAPIBaseView, generics.RetrieveUpdateAPIView):
     view_name = 'user-provider-subscription-detail'
     view_category = 'subscriptions'
+    serializer_class = UserProviderSubscriptionSerializer
 
     def get_object(self):
-        import ipdb
-        ipdb.set_trace()
         provider_id = self.kwargs['provider_id']
+        user_id = self.kwargs['user_id']
+        user = OSFUser.load(user_id)
+        if not user:
+            raise NotFound('User with id {} cannot be found.'.format(user_id))
         provider = PreprintProvider.objects.get(_id=provider_id)
         notification = NotificationSubscription.objects.get(provider=provider)
+        subscribers = notification.none.all() | notification.email_transactional.all() | notification.email_digest.all()
+        if user not in subscribers:
+            raise NotFound('{} cannot be found in the list of subscribers.'.format(user))
+
         return notification
+
